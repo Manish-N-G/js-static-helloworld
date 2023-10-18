a03e6de921a2cc9ce5c4bc40817f8d1c8f21aac7
Manish-N-G <rustieee@gmail.com>
2023-10-18 15:04:42 +0200

    added button

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Your JavaScript code goes here
     console.log('Hello from script.js!');
+
+    // Function to generate a random number
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 100) + 1;
+    }
+
+    // Get references to button and display elements
+    const button = document.getElementById('randomNumberButton');
+    const display = document.getElementById('randomNumberDisplay');
+
+    // Add click event listener to the button
+    button.addEventListener('click', function() {
+        // Generate a random number and display it
+        const randomNumber = generateRandomNumber();
+        display.textContent = `Random Number: ${randomNumber}`;
+    });
 });
